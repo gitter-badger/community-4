39770e0290b5b6c36441b2b17b08764b1ce215d2
Anthony Lapenna <lapenna.anthony@gmail.com>
2016-03-30 20:12:48 +1300

    Automatically center the view on current position in map using geolocation.

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
 import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 
-const WorldMap = function worldMap() {
-  return (
-      <section style={{ position: 'absolute', height: '100%', width: '100%' }}>
-        <GoogleMapLoader
-          containerElement={ <div style={{ height: '100%' }} /> }
-          googleMapElement={
-            <GoogleMap defaultZoom={3} defaultCenter={{ lat: -25.363882, lng: 131.044922 }} />
-          }
-        />
-      </section>
-    );
-};
+import BaseComponent from './common/BaseComponent';
+
+const geolocation = (
+  navigator.geolocation || {
+    getCurrentPosition: (success, failure) => {
+      failure('Your browser doesn\'t support geolocation.');
+    },
+  }
+);
+
+class WorldMap extends BaseComponent {
+
+  constructor() {
+    super();
+    this.state = {
+      center: null,
+    };
+  }
+
+  componentDidMount() {
+    geolocation.getCurrentPosition((position) => {
+      this.setState({
+        center: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        },
+      });
+    }, (reason) => {
+      console.log(`Unable to retrieve current position: ${reason}`);
+    });
+  }
+
+  render() {
+    return (
+        <section style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <GoogleMapLoader
+            containerElement={ <div style={{ height: '100%' }} /> }
+            googleMapElement={
+              <GoogleMap defaultZoom={12} center={this.state.center} />
+            }
+          />
+        </section>
+      );
+  }
+}
 
 export default WorldMap;
